ce693da2a413aab5531c1a7b2c4751c0c80f13a3
Alim Mambetov <alim.mambetov.8866@gmail.com>
2025-07-28 17:30:39 +0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import path from 'path';
 
 const nextConfig: NextConfig = {
   poweredByHeader: false,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-    prependData: `@import "~@/styles/global/index.scss";`, // Автоматически добавляет миксины
-  },
+  // sassOptions: {
+  //   includePaths: [path.join(__dirname, 'styles')],
+  //   prependData: `@import "~@/styles/global/index.scss";`, // Автоматически добавляет миксины
+  // },
 };
 
 export default nextConfig;
